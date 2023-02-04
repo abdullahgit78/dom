854287a1d58885a3556e64c76b7b44ee385e3403
abd <abdullh@emailcom>
2023-02-04 20:42:47 +0300

    this is commit

---
+++ main.js
@@ -0,0 +1,178 @@
+var mydiv= document.createElement('div');
+var myDiv1 = document.createElement('div')
+var myimage =document.createElement('img');
+myimage.setAttribute("src","img/a1.jpeg");
+var myh1 =document.createElement('h4');
+mytext1=document.createTextNode('مشاوي');
+
+
+myDiv1.appendChild(myimage);
+myh1.appendChild(mytext1);
+myDiv1.appendChild(myh1);
+mydiv.appendChild(myDiv1);
+document.body.appendChild(mydiv);
+
+mydiv.style.display = 'flex';
+mydiv.style.justifyContent = 'space-between';
+mydiv.style.flexWrap = 'wrap';
+mydiv.style.backgroundColor="black";
+myimage.style.width="100%";
+
+myh1.style.color = "white";
+myh1.style.backgroundColor = "rgb(230, 174, 20)";
+myh1.style.fontSize = "20px";
+myh1.style.textAlign = "center";
+myh1.style.marginTop = "5px";
+myimage.style.borderRadius="20PX 20PX 0 0";
+myh1.style.borderRadius="0 0 20px 20px";
+
+
+
+
+////d1
+var myDiv1 = document.createElement('div')
+var myimage =document.createElement('img');
+myimage.setAttribute("src","img/a2.jpeg");
+var myh1 =document.createElement('h4');
+mytext1=document.createTextNode('كبب');
+
+
+myDiv1.appendChild(myimage);
+myh1.appendChild(mytext1);
+myDiv1.appendChild(myh1);
+mydiv.appendChild(myDiv1);
+document.body.appendChild(mydiv);
+
+mydiv.style.display = 'flex';
+mydiv.style.justifyContent = 'space-between';
+mydiv.style.flexWrap = 'wrap';
+
+myimage.style.width="100%";
+
+myh1.style.color = "white";
+myh1.style.backgroundColor = "rgb(230, 174, 20)";
+myh1.style.fontSize = "20px";
+myh1.style.textAlign = "center";
+myh1.style.marginTop = "5px";
+myimage.style.borderRadius="20PX 20PX 0 0";
+myh1.style.borderRadius="0 0 20px 20px";
+
+//d2
+var myDiv1 = document.createElement('div')
+var myimage =document.createElement('img');
+myimage.setAttribute("src","img/a4.jpeg");
+var myh1 =document.createElement('h4');
+mytext1=document.createTextNode('رزوفروج');
+
+
+myDiv1.appendChild(myimage);
+myh1.appendChild(mytext1);
+myDiv1.appendChild(myh1);
+mydiv.appendChild(myDiv1);
+document.body.appendChild(mydiv);
+
+mydiv.style.display = 'flex';
+mydiv.style.justifyContent = 'space-between';
+mydiv.style.flexWrap = 'wrap';
+
+myimage.style.width="100%";
+
+myh1.style.color = "white";
+myh1.style.backgroundColor = "rgb(230, 174, 20)";
+myh1.style.fontSize = "20px";
+myh1.style.textAlign = "center";
+myh1.style.marginTop = "5px";
+myimage.style.borderRadius="20PX 20PX 0 0";
+myh1.style.borderRadius="0 0 20px 20px";
+
+//d3
+var myDiv1 = document.createElement('div')
+var myimage =document.createElement('img');
+myimage.setAttribute("src","img/a5.jpeg");
+var myh1 =document.createElement('h4');
+mytext1=document.createTextNode('مندي');
+
+
+myDiv1.appendChild(myimage);
+myh1.appendChild(mytext1);
+myDiv1.appendChild(myh1);
+mydiv.appendChild(myDiv1);
+document.body.appendChild(mydiv);
+
+mydiv.style.display = 'flex';
+mydiv.style.justifyContent = 'space-between';
+mydiv.style.flexWrap = 'wrap';
+
+myimage.style.width="100%";
+
+myh1.style.color = "white";
+myh1.style.backgroundColor = "rgb(230, 174, 20)";
+myh1.style.fontSize = "20px";
+myh1.style.textAlign = "center";
+myh1.style.marginTop = "5px";
+myimage.style.borderRadius="20PX 20PX 0 0";
+myh1.style.borderRadius="0 0 20px 20px";
+
+//d4
+var myDiv1 = document.createElement('div')
+var myimage =document.createElement('img');
+myimage.setAttribute("src","img/a6.jpeg");
+var myh1 =document.createElement('h4');
+mytext1=document.createTextNode('فروج مشوي');
+
+
+myDiv1.appendChild(myimage);
+myh1.appendChild(mytext1);
+myDiv1.appendChild(myh1);
+mydiv.appendChild(myDiv1);
+document.body.appendChild(mydiv);
+
+mydiv.style.display = 'flex';
+mydiv.style.justifyContent = 'space-between';
+mydiv.style.flexWrap = 'wrap';
+
+myimage.style.width="100%";
+
+myh1.style.color = "white";
+myh1.style.backgroundColor = "rgb(230, 174, 20)";
+myh1.style.fontSize = "20px";
+myh1.style.textAlign = "center";
+myh1.style.marginTop = "5px";
+myimage.style.borderRadius="20PX 20PX 0 0";
+myh1.style.borderRadius="0 0 20px 20px";
+
+//d5
+var myDiv1 = document.createElement('div')
+var myimage =document.createElement('img');
+myimage.setAttribute("src","img/a7.jpeg");
+var myh1 =document.createElement('h4');
+mytext1=document.createTextNode('كبة بسياخ');
+
+
+myDiv1.appendChild(myimage);
+myh1.appendChild(mytext1);
+myDiv1.appendChild(myh1);
+mydiv.appendChild(myDiv1);
+document.body.appendChild(mydiv);
+
+mydiv.style.display = 'flex';
+mydiv.style.justifyContent = 'space-between';
+mydiv.style.flexWrap = 'wrap';
+
+myimage.style.width="100%";
+
+myh1.style.color = "white";
+myh1.style.backgroundColor = "rgb(230, 174, 20)";
+myh1.style.fontSize = "20px";
+myh1.style.textAlign = "center";
+myh1.style.marginTop = "5px";
+myimage.style.borderRadius="20PX 20PX 0 0";
+myh1.style.borderRadius="0 0 20px 20px";
+
+
+
+
+
+
+
+
